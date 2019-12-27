2272f5d885cc16a5dfe5f0e6d86dd7f25bd6cc97
Anton Malofeev <arenukvern@gmail.com>
2019-12-28 01:29:56 +0300

    Full SheetElement to ts refactor

--- src/LogicCore/Instances/SheetElement/SheetElement.ts
+++ src/LogicCore/Instances/SheetElement/SheetElement.ts
@@ -1,141 +1,146 @@
-import { Basic } from './Basic'
+import { Basic } from "./Basic";
 
-export class SheetElement extends Basic implements SheetElementsInterface.SheetElement{
-    // #region Properties (9)
+export class SheetElement extends Basic
+  implements SheetElementsInterface.SheetElement {
+  // #region Properties (5)
 
-    private _excelSheetName: string
-    private _regExpNumeration: RegExp = /(.\d_\d.)/g
+  private _excelSheetName: string;
+  private _regExpNumeration: RegExp = /(.\d_\d.)/g;
 
-    public delimiter: string = "_"
-    public elements: SheetElementsInterface.EMap
-    public positions: SheetElementsInterface.Positions = {
-      first: 0,
-      second: 0,
-    }
+  public delimiter: string = "_";
+  public elements: SheetElementsInterface.EMap;
+  public positions: SheetElementsInterface.Positions = {
+    first: 0,
+    second: 0
+  };
 
-    // #endregion Properties (9)
+  // #endregion Properties (5)
 
-    // #region Constructors (1)
+  // #region Constructors (1)
 
-    constructor(
-      {
-        id, name, isVisible, color,
-        typeOfName, positions,
-        delimiter, elements,
-      }: SheetElementsInterface.SheetElementConstructor
-    ) {
-      super({id,name,isVisible,color,typeOfName})
-      this._excelSheetName = name
-      this.positions = positions
-      if(delimiter) this.delimiter= delimiter
-      this.elements = elements === undefined ? new Map() : elements
-    }
+  constructor({
+    id,
+    name,
+    isVisible,
+    color,
+    typeOfName,
+    positions,
+    delimiter,
+    elements
+  }: SheetElementsInterface.SheetElementConstructor) {
+    super({
+      id,
+      name,
+      isVisible,
+      color,
+      typeOfName,
+      _classTitle: "SheetElement"
+    });
+    this._excelSheetName = name;
+    this.positions = positions;
+    if (delimiter) this.delimiter = delimiter;
+    this.elements = elements === undefined ? new Map() : elements;
+  }
 
-    // #endregion Constructors (1)
+  // #endregion Constructors (1)
 
-    // #region Public Accessors (2)
+  // #region Public Accessors (2)
 
-    public get name(): string{
-      try {
-        const name: string = this[this.typeOfName]
-        return name
-      } catch (error) {
-        return ""
-      }
-    }
-
-    public set name(value: string){
-      try {
-        this[this.typeOfName] = value
-      } catch (error) {
-      }
-    }
-
-    // #endregion Public Accessors (2)
-
-    // #region Private Accessors (4)
-
-    private get _decodedName(): string {
-      try {
-        const sentence = this._excelSheetName
-        const cleanSentence: string = sentence.replace(this._regExpNumeration, "")
-        return cleanSentence
-      } catch (error) {
-        return ""
-      }
-    }
-
-    private set _decodedName(value: string) {
-      try {
-        this._excelSheetName = value
-      } catch (error) {
-        this._excelSheetName = ""
-      }
-    }
-
-    /** 1. extract possible pattern -> 00_00 but it can be e0_0uio,
-     * so it is necessary to clean up after match and separate to two numbers
-     * 2. to clean up pattern 0_0 and all numbers in name
-     * 3. create new name 
-     */
-    private get _encodedName(): string {
-      try {
-        const cleanSentence: string = this._decodedName
-        const readyPattern: string = this._numerationPattern()
-
-        return cleanSentence + readyPattern
-      } catch (error) {
-        return  ""
-      }
-    }
-
-    private set _encodedName(value: string) {
-      try {
-        const readyPattern: string = this._numerationPattern()
-        this._excelSheetName = value + readyPattern
-      } catch (error) {
-        this._excelSheetName =''
-      }
-    }
-
-    // #endregion Private Accessors (4)
-
-    // #region Private Methods (2)
-
-    private _doesNameIncludesNumerationPattern(): boolean {
-      try {
-        const result = this._excelSheetName.match(this._regExpNumeration)
-        if(Array.isArray(result) && result.length>0) return true
-        return false
-      } catch (error) {
-        console.log("_doesNameIncludesNumerationPattern", error)
-        return false
-      }
-    }
-
-    private _numerationPattern(): string {
-      try {
-        function returnPart(draftValue: number): string{
-          return draftValue > 9 ? String(draftValue) : "0" + String(draftValue)
-        }
-        const firstPart: string = returnPart(this.positions.firstNumber)
-        const secondPart: string = returnPart(this.positions.secondNumber)
-        const readyPattern: string = firstPart + this.delimiter + secondPart
-
-        return readyPattern
-      
-      } catch (error) {
-        return ""
-      }
-    }
-
-    // #endregion Private Methods (2)
-}
-  export const SheetElementConfig = {
-    typeOfName: {
-      originalName: "_excelSheetName", 
-      simpleName: "_decodedName",
-      numeratedName: "_encodedName",
+  public get name(): string {
+    try {
+      const name: string = this[this.typeOfName];
+      return name;
+    } catch (error) {
+      throw this.log.error("name get", error);
     }
   }
+
+  public set name(value: string) {
+    try {
+      this[this.typeOfName] = value;
+    } catch (error) {
+      throw this.log.error("name set", error);
+    }
+  }
+
+  // #endregion Public Accessors (2)
+
+  // #region Private Accessors (4)
+
+  private get _decodedName(): string {
+    try {
+      const sentence = this._excelSheetName;
+      const cleanSentence: string = sentence.replace(
+        this._regExpNumeration,
+        ""
+      );
+      return cleanSentence;
+    } catch (error) {
+      throw this.log.error("_decodedName get", error);
+    }
+  }
+
+  private set _decodedName(value: string) {
+    try {
+      this._excelSheetName = value;
+    } catch (error) {
+      throw this.log.error("_decodedName set", error);
+    }
+  }
+
+  /** 1. extract possible pattern -> 00_00 but it can be e0_0uio,
+   * so it is necessary to clean up after match and separate to two numbers
+   * 2. to clean up pattern 0_0 and all numbers in name
+   * 3. create new name
+   */
+  private get _encodedName(): string {
+    try {
+      const cleanSentence: string = this._decodedName;
+      const readyPattern: string = this._numerationPattern();
+
+      return cleanSentence + readyPattern;
+    } catch (error) {
+      throw this.log.error("_encodedName get", error);
+    }
+  }
+
+  private set _encodedName(value: string) {
+    try {
+      const readyPattern: string = this._numerationPattern();
+      this._excelSheetName = value + readyPattern;
+    } catch (error) {
+      throw this.log.error("_encodedName set", error);
+    }
+  }
+
+  // #endregion Private Accessors (4)
+
+  // #region Private Methods (2)
+
+  private _doesNameIncludesNumerationPattern(): boolean {
+    try {
+      const result = this._excelSheetName.match(this._regExpNumeration);
+      if (Array.isArray(result) && result.length > 0) return true;
+      return false;
+    } catch (error) {
+      throw this.log.error("_doesNameIncludesNumerationPattern", error);
+    }
+  }
+
+  private _numerationPattern(): string {
+    try {
+      const returnPart = (draftValue: number): string => {
+        return draftValue > 9 ? String(draftValue) : "0" + String(draftValue);
+      };
+      const firstPart: string = returnPart(this.positions.first);
+      const secondPart: string = returnPart(this.positions.second);
+      const readyPattern: string = firstPart + this.delimiter + secondPart;
+
+      return readyPattern;
+    } catch (error) {
+      throw this.log.error("_numerationPattern", error);
+    }
+  }
+
+  // #endregion Private Methods (2)
 }
