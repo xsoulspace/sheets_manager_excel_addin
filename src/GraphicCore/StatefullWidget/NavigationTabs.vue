94c2823a095240db12291e20b5627892fbabfb0b
Anton Malofeev <arenukvern@gmail.com>
2020-01-19 00:17:24 +0300

    Started Dark theme  #28

--- src/GraphicCore/StatefullWidget/NavigationTabs.vue
+++ src/GraphicCore/StatefullWidget/NavigationTabs.vue
@@ -1,13 +1,20 @@
 <template>
 	<div class="tabs">
-		<div class="tabs__tab">
-			<div class="search"
+		<div
+			class="tabs__tab"
+			@mouseover="changeIsSearchActive(true)"
+			@mouseleave="changeIsSearchActive(false)"
+			@click="changeIsSearchActive(true, true)"				
+			:class="{
+				'--is-dark': isDarkTheme,
+			}"
+		>
+			<div
+				class="search"
 				:class="{
 					'--is-open': isSearchActive,
+					'--is-dark': isDarkTheme,
 				}"
-				@mouseover="changeIsSearchActive(true)"
-				@mouseleave="changeIsSearchActive(false)"
-				@click="changeIsSearchActive(true, true)"
 			>
 				<input
 					v-model="searchingWord"
@@ -21,56 +28,53 @@
 				</div>
 			</div>
 		</div>
-
-		<!-- <transition name="fade">
-			<div class="tabs__tab">
-				<li v-show="!isSearchActive" @click="isSettingsActive = true">
-					<a>
-						<span class="icon">
-							<i class="fas fa-sliders-h"></i>
-						</span>
-					</a>
-				</li>
+		<transition name="fade">
+			<div class="tabs__tab" @click="turnOnInfo">
+				<div
+					class="button"
+					:class="{
+						'--is-dark': isDarkTheme,
+					}"
+				>
+					<span class="icon">
+						<i class="fa fa-refresh"></i>
+					</span>
+				</div>
 			</div>
 		</transition>
-
-			<transition name="fade">
-		<div class="tabs__tab">
-				<li v-show="!isSearchActive">
-					<a>
-						<span class="icon">
-							<i class="fas fa-question"></i>
-						</span>
-					</a>
-				</li>
-
-		</div>
-
-			</transition>
-			<transition name="fade">
-		<div class="tabs__tab">
-				<li v-show="!isSearchActive">
-					<a>
-						<span class="icon">
-							<i class="fas fa-info"></i>
-						</span>
-					</a>
-				</li>
-
-		</div>
-
-			</transition>
-			<transition name="fade">
-				<div class="tabs__tab">
-				<li v-show="!isSearchActive">
-					<a @click="refreshSheets">
-						<span class="icon">
-							<i class="fa fa-refresh"></i>
-						</span>
-					</a>
-				</li>
+		<transition name="fade">
+			<div class="tabs__tab" @click="turnOnSettings">
+				<div class="button" 				:class="{
+					'--is-dark': isDarkTheme,
+				}">
+					<span class="icon">
+						<i class="fas fa-sliders-h"></i>
+					</span>
 				</div>
-			</transition> -->
+			</div>
+		</transition>
+		<transition name="fade">
+			<div class="tabs__tab" @click="turnOnTutorial">
+				<div class="button" 				:class="{
+					'--is-dark': isDarkTheme,
+				}">
+					<span class="icon">
+						<i class="fas fa-question"></i>
+					</span>
+				</div>
+			</div>
+		</transition>
+		<transition name="fade">
+			<div class="tabs__tab" @click="turnOnInfo">
+				<div class="button" 				:class="{
+					'--is-dark': isDarkTheme,
+				}">
+					<span class="icon">
+						<i class="fas fa-info"></i>
+					</span>
+				</div>
+			</div>
+		</transition>
 	</div>
 </template>
 <script lang="ts">
@@ -82,14 +86,14 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class NavigationTabs extends Vue {
 	isSearchActive: boolean = false
 	searchingWord: string = ''
-	
+	turnOnInfo() {}
 	turnOnSettings() {}
+	turnOnTutorial() {}
 	async refreshSheets() {}
 	changeIsSearchActive(value: boolean, useItExplicitly?: boolean) {
 		let isExplicit: boolean = false
 		if (useItExplicitly === true) isExplicit = true
 		if (this.searchingWord.length > 0) isExplicit = true
-		console.log(isExplicit)
 		if (isExplicit) {
 			this.$data.isSearchActive = isExplicit
 			return
@@ -98,32 +102,4 @@ export default class NavigationTabs extends Vue {
 	}
 }
 </script>
-<style lang="scss" scoped>
-.tabs {
-	&.has-tiny-paddings {
-		padding-bottom: 0.4rem;
-		padding-top: 0.2rem;
-		padding-left: 0.1rem;
-		padding-right: 0.1rem;
-	}
-	&.is-overflowless-horizontal {
-		overflow-x: hidden;
-	}
-
-	li {
-		transition-property: width;
-		transition-duration: 0.6s;
-		transition-timing-function: ease-in-out;
-		&.fade-enter-active {
-			transition: opacity 1s;
-			transition-delay: 0.5s;
-		}
-		&.fade-leave-active {
-			transition: opacity 0.1s;
-		}
-		&.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-			opacity: 0;
-		}
-	}
-}
-</style>
+<style lang="scss" scoped></style>
