d55b90181144febfb234bd4d9155d3b43354b44d
Mihail <mix544@ya.ru>
2021-08-04 02:05:45 +0300

    feat: ant design. Setup is a wip

--- src/consts/app_router.ts
+++ src/consts/app_router.ts
@@ -1,2 +1,14 @@
-// TODO: implement AppRouter
-export const AppRouter = {}
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import home from '#/screens/home.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: home,
+  },
+]
+
+export const appRouter = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 /// <reference types="@types/office-js" />
-import { AppRouter } from '#/consts'
-import { App } from '#/widgets/app'
-import '@xsoulspace/vuefer/style.css'
+import { appRouter } from '#/consts'
+import App from '#/widgets/app.vue'
+
 import { createApp } from 'vue'
 import './registerServiceWorker'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).use(AppRouter).mount('#app')
+const app = createApp(App)
+app.use(appRouter).use(Antd).mount('#app')
+
+// app.config['productionTip'] = false
