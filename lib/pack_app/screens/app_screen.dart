4263b128b8dc00b4ed4d399972b1e2f647a9e925
Anton Malofeev <anton@xsoulspace.dev>
2022-05-22 15:04:11 +0300

    feat: structure

---
+++ lib/pack_app/screens/app_screen.dart
@@ -0,0 +1,11 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const FluentApp();
+  }
+}
