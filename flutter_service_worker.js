287598115292040143ff8a4dcd50c10d67feca0d
Arenukvern <Arenukvern@users.noreply.github.com>
2023-01-12 01:35:49 +0000

    deploy: 0941f710eb4ba3018d171e8fe47368ec25343e6c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "33605fb1023faf1801aa6cc6f2b270bc",
-"index.html": "7f0dc53b955fcda3f45f4b72ccb29efb",
-"/": "7f0dc53b955fcda3f45f4b72ccb29efb",
-"main.dart.js": "a58ac8cf2d4c420f85c6d032cd0c9383",
+  "version.json": "780b0c09b4758176ecfeaa4615648d14",
+"index.html": "afa012793d8f7544eecf1fe5465bd2eb",
+"/": "afa012793d8f7544eecf1fe5465bd2eb",
+"main.dart.js": "634270b680253a61d6d3764dedd8900d",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "bd0816afa90451563260fe6be482b84d",
 "icons/icon-16.png": "e037accab69f99f00cd14cf7702d6443",
