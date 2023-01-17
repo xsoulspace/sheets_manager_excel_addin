268de886852a8d6c414665a6dcd8f05fdbb3cacd
Anton Malofeev <arenukvern@gmail.com>
2023-01-17 21:08:01 +0200

    fix: platform run

--- lib/pack_sheets/widgets/ads_banner.dart
+++ lib/pack_sheets/widgets/ads_banner.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:yandex_ads_sdk/yandex_ads_sdk.dart';
 
@@ -7,8 +8,8 @@ class AdsBanner extends HookWidget {
 
   @override
   Widget build(final BuildContext context) {
+    if (!kIsWeb) return const SizedBox();
     final screenSize = MediaQuery.of(context).size;
-
     return YandexFlexibleBanner(
       adUnitId: 'R-A-2140277-2',
       height: 80,
